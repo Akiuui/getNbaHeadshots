727a1dcc4c755bb2936fe85254ab5b88a7c13e53
Aleksa Stojanovic <aleksastojanovic18@gmail.com>
2025-02-23 00:28:40 +0100

    Added logging

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
 from flask import Flask, request, jsonify
+import os
+import logging
 from nba_api.stats.static import players
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+
 @app.route("/")
 def getPlayer():
-    fullName = request.args.get("fullName").lower().strip()
+    fullName = request.args.get("fullName")
 
+    if fullName is None:
+        logging.error("The arg 'fullName' is not entered")
+        return jsonify({"Error":"The arg 'fullName' is not entered"})
+    else:
+        logging.info(f"The args is fullName: {fullName}")
+    
+    fullName = fullName.lower().strip()
+    logging.info("Trying to find a player with his name")
     player = players.find_players_by_full_name(fullName)
+    logging.info("Found the player")
 
     if len(player) > 1:
+        logging.error("There are multiple players with same name")
         return jsonify({"Exception":"There are multiple players with same name"})
 
     if len(player) < 1:
+        logging.error(" with that name not found")
         return jsonify({"Error":"Player with that name not found"})
 
     player = player[0]
@@ -22,6 +38,7 @@ def getPlayer():
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, port=8007)
-    # app.run(debug=False)
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT", 8006))
+    serve(app, host="0.0.0.0", port=port)
 
